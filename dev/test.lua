d5dbbc8a424fecd5ff3993d7a5a8a0f7dae5fa17
kevin <2399695400@qq.com>
2018-01-09 14:45:13 +0800

    hello

---
+++ dev/test.lua
@@ -0,0 +1,6 @@
+--layout->setBackGroundColorType(LAYOUT_COLOR_SOLID);
+--layout->setBackGroundColor(Color3B::RED)
+
+-- Layout显示底色
+layout:setBackGroundColorType(ccui.LayoutBackGroundColorType.solid)
+layout:setBackGroundColor(cc.RED)
