b23f757058fe81d066a5ddae5623a728af41f67f
Rock <2399695400@qq.com>
2018-10-27 12:04:54 +0800

    Update test2.lua
    luajit ffi测量数量长度方法测试

--- test/test2.lua
+++ test/test2.lua
@@ -15,17 +15,54 @@ ffi.cdef [[
 		#pragma pack(4)
 		typedef struct {
 			unsigned short KindId;
-            unsigned char TestBuf[32];
+			unsigned char TestBuf[32];
 		} st_Regist;
-    ]]
+	]]
     
 local str = ffi.new("st_Regist")
 str.KindId = 5
-str.TestBuf = "helloÄãºÃ"
+str.TestBuf = "helloÃ„Ã£ÂºÃƒ"
 print("ffi test", str.KindId, ffi.string(str.TestBuf, 32))
 
 
+ffi.cdef [[
+    typedef struct {
+        int a;
+        unsigned short b;
+        char c[5];
+        int d[5];
+        uint64_t ss[7][8];
+        long long e[7][8];
+        long f;
+        double g;
+        float h;
+    } Test;
+]]
 
+local c = ffi.new("Test")
+ffi.fill(c, ffi.sizeof(c), 0)
+--[[c.a = 13
+c.b = 78
+c.c[0] = 12
+c.c[1] = 13
+c.d[0] = 56]]
+
+
+local function get_array_width(array_cdata)
+    local typeof = tostring(ffi.typeof(array_cdata))
+    local typedef = string.gsub(typeof, "^ctype<([%w _]+) %(&%)[%d%[%]]+>$", "%1")
+    assert(typeof ~= typedef, typedef)
+    return ffi.sizeof(typedef)
+end
+
+local function get_array_length(array_cdata)
+    return ffi.sizeof(array_cdata) / get_array_width(array_cdata)
+end
+
+print("---->>", ffi.sizeof(c.ss))
+print("====>>", get_array_width(c.ss))
+
+print("****>>", get_array_length(c.ss))
 
 
 
